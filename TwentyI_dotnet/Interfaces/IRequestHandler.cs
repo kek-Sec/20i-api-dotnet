ac0acc124f8e9d319e00ea098a387c5c25b31133
kek-Sec <georgepetrakis99@gmail.com>
2022-05-24 16:21:02 +0300

    Update the interface aswell

--- TwentyI_dotnet/Interfaces/IRequestHandler.cs
+++ TwentyI_dotnet/Interfaces/IRequestHandler.cs
@@ -9,40 +9,36 @@
 public interface IRequestHandler
 {
     /// <summary>
-    /// GetAsync is a generic method that will perform a GET request to the 20i api.
+    /// GetSync is a method that will perform a GET request to the 20i api.
     /// </summary>
-    /// <typeparam name="T">The type of object that will be returned from the request.</typeparam>
     /// <param name="url">The url to perform the request to.</param>
     /// <param name="token">The bearer token to use for the request.</param>
-    /// <returns>A Task that will return the object that was returned from the request.</returns>
-    Task<T?> GetAsync<T>(string url, string token);
+    /// <returns>The json string returned from the 20i API</returns>
+    string GetSync(string url, string token);
 
     /// <summary>
-    /// PostAsync is a generic method that will perform a POST request to the 20i api.
+    /// PostSync is a method that will perform a POST request to the 20i api.
     /// </summary>
-    /// <typeparam name="T">The type of object that will be returned from the request.</typeparam>
     /// <param name="url">The url to perform the request to.</param>
     /// <param name="token">The bearer token to use for the request.</param>
     /// <param name="body">The body of the request.</param>
-    /// <returns>A Task that will return the object that was returned from the request.</returns>
-    Task<T?> PostAsync<T>(string url, string token, object body);
+    /// <returns>The json string returned from the 20i api</returns>
+     string PostSync(string url, string token, object body);
 
     /// <summary>
-    /// GetSync is a generic method that will perform a GET request to the 20i api.
+    /// GetAsync is a method that will perform a GET request to the 20i api.
     /// </summary>
-    /// <typeparam name="T">The type of object that will be returned from the request.</typeparam>
     /// <param name="url">The url to perform the request to.</param>
     /// <param name="token">The bearer token to use for the request.</param>
-    /// <returns>The object that was returned from the request.</returns>
-    T? GetSync<T>(string url, string token);
+    /// <returns>The json response string.</returns>
+    Task<string> GetAsync(string url, string token);
 
     /// <summary>
-    /// PostSync is a generic method that will perform a POST request to the 20i api.
+    /// PostAsync is a method that will perform a POST request to the 20i api.
     /// </summary>
-    /// <typeparam name="T">The type of object that will be returned from the request.</typeparam>
     /// <param name="url">The url to perform the request to.</param>
     /// <param name="token">The bearer token to use for the request.</param>
     /// <param name="body">The body of the request.</param>
-    /// <returns>The object that was returned from the request.</returns>
-    T? PostSync<T>(string url, string token, object body);
+    /// <returns>The json string returned from the 20i api.</returns>
+    Task<string> PostAsync(string url, string token, object body);
 }
